3420ac626ada832d8d12ae7677c2bea354553527
Anthony <736252868@qq.com>
2019-06-30 10:48:00 +0800

    验证码

--- src/main/java/com/anthonyzero/scaffold/system/controller/ViewController.java
+++ src/main/java/com/anthonyzero/scaffold/system/controller/ViewController.java
@@ -1,15 +1,24 @@
 package com.anthonyzero.scaffold.system.controller;
 
+import com.anthonyzero.scaffold.common.core.SysConstant;
+import com.anthonyzero.scaffold.common.exception.RedisConnectException;
 import com.anthonyzero.scaffold.common.utils.CaptchaUtil;
+import com.anthonyzero.scaffold.system.service.RedisService;
 import com.wf.captcha.Captcha;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    private RedisService redisService;
 
     @GetMapping("/login")
     public String login(HttpServletRequest request) {
@@ -23,8 +32,10 @@ public class ViewController {
      * @throws Exception
      */
     @GetMapping("/images/captcha")
-    public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws IOException, RedisConnectException {
+        HttpSession session = request.getSession();
         String code = CaptchaUtil.outPng(110, 34, 4, Captcha.TYPE_NUM_AND_UPPER, request, response);
-        System.out.println(code);
+        String key = SysConstant.CODE_PREFIX + session.getId();
+        redisService.set(key, code, 120);
     }
 }
