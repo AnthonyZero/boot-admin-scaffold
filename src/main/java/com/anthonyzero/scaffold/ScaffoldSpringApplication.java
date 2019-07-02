18a0567b933834575a9ab0287252a4842cfaab5e
pingjin <736252868@qq.com>
2019-07-02 15:14:53 +0800

    springboot会自动配置事务TransactionAutoConfiguration 无需在启动类加上@EnableTransactionManagement

--- src/main/java/com/anthonyzero/scaffold/ScaffoldSpringApplication.java
+++ src/main/java/com/anthonyzero/scaffold/ScaffoldSpringApplication.java
@@ -1,9 +1,11 @@
 package com.anthonyzero.scaffold;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
+@MapperScan("com.anthonyzero.scaffold.*.mapper")
 public class ScaffoldSpringApplication {
 
     public static void main(String[] args) {
